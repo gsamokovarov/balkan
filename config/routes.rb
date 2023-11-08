db5123baa1f6cb749b3e2f810736887c8069999a
Genadi Samokovarov <gsamokovarov@gmail.com>
2023-11-08 17:30:56 +0200

    Initial broken layout

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "home#show"
+
+  direct(:twitter) { "https://twitter.com/@balkanruby" }
+  direct(:youtube) { "https://www.youtube.com/@balkanruby6171" }
 end
