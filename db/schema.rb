63007699ab8bdbffec9669ccddd6d88e1c775537
Genadi Samokovarov <gsamokovarov@gmail.com>
2023-11-12 10:59:44 +0200

    No more ticket tracking in our system
    The tickets will be in stripe for now, no need to keep data in our
    system. Maybe the ticket types, but that's it.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_11_100434) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_11_100125) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  # Custom types defined in this database.
-  # Note that some types may not work with other database engines. Be careful if changing database.
-  create_enum "shirt_size", ["XS", "S", "M", "L", "XL", "XXL"]
 
   create_table "events", force: :cascade do |t|
     t.string "name", null: false
@@ -25,16 +21,4 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_11_100434) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "tickets", force: :cascade do |t|
-    t.bigint "event_id", null: false
-    t.string "name"
-    t.enum "shirt_size", enum_type: "shirt_size"
-    t.boolean "childcare", default: false, null: false
-    t.decimal "price", default: "0.0", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["event_id"], name: "index_tickets_on_event_id"
-  end
-
-  add_foreign_key "tickets", "events"
 end
