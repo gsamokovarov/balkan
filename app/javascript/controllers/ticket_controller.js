121f85b6bff05285313e0c218adf39c5a57c00d0
Genadi Samokovarov <gsamokovarov@gmail.com>
2023-11-22 10:43:04 +0200

    Show how many tickets are about to be bought

--- app/javascript/controllers/ticket_controller.js
+++ app/javascript/controllers/ticket_controller.js
@@ -2,9 +2,16 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="ticket"
 export default class extends Controller {
-  static targets = ["tickets", "template"]
+  static targets = ["tickets", "template", "buyText"]
+  static values = { count: { type: Number, default: 1 } }
 
   add() {
+    this.countValue++
     this.ticketsTarget.insertAdjacentHTML("beforeend", this.templateTarget.outerHTML)
+  }
+
+  countValueChanged() {
+    console.log(this.countValue)
+    this.buyTextTarget.textContent = `Buy ${this.countValue} tickets`
   }
 }
