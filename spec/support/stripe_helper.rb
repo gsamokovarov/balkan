922e38571fb9bfdbe697bd8006494e443a984fdd
Svetlozar Mihailov <svetliomihailov@gmail.com>
2023-11-25 21:26:43 +0200

    Apply 10% discount to stripe for more than 3 tickets

--- spec/support/stripe_helper.rb
+++ spec/support/stripe_helper.rb
@@ -1,17 +1,17 @@
 module SpecSupport
   module StripeHelper
-    def stub_stripe_checkout(ticket_type, tickets_params, session_id:, session_url:)
+    def stub_stripe_checkout(expected_line_items, session_id:, session_url:)
       stub(Stripe::Checkout::Session).to receive(:create).with(
-        stripe_checkout_opts(ticket_type, tickets_params)
+        stripe_checkout_opts(expected_line_items)
       ).and_return(OpenStruct.new({
         id: session_id,
         url: session_url
       }))
     end
 
-    def stub_stripe_checkout_with_invoice(ticket_type, tickets_params, session_id:, session_url:)
+    def stub_stripe_checkout_with_invoice(expected_line_items, session_id:, session_url:)
       stub(Stripe::Checkout::Session).to receive(:create).with(
-        stripe_checkout_opts(ticket_type, tickets_params).merge({
+        stripe_checkout_opts(expected_line_items).merge({
           billing_address_collection: 'required',
           tax_id_collection: {
             enabled: true
@@ -23,18 +23,18 @@ module SpecSupport
       }))
     end
 
-    def stripe_checkout_opts(ticket_type, tickets_params)
+    def stripe_checkout_opts(expected_line_items)
       {
         currency: "eur",
         success_url: "http://example.com/thanks",
         cancel_url: "http://example.com/",
-        line_items: tickets_params.map do |ticket_params|
+        line_items: expected_line_items.map do |line_item|
           {
             price_data: {
               currency: "eur",
-              unit_amount: (ticket_type.price * 100).to_i,
+              unit_amount: (line_item[:price] * 100).to_i,
               product_data: {
-                name: "#{ticket_type.type} - #{ticket_params[:name]}",
+                name: line_item[:description],
               },
             },
             quantity: 1,
