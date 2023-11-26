b44e395cf2623511f473ec5cb757ab4767f024fd
Genadi Samokovarov <gsamokovarov@gmail.com>
2023-11-26 22:23:10 +0200

    xUnitify the feature specs

--- spec/features/buy_tickets_spec.rb
+++ spec/features/buy_tickets_spec.rb
@@ -3,17 +3,14 @@ require 'rails_helper'
 RSpec.case "Buy tickets", type: :feature do
   test "redirects the user to Stripe checkout" do
     stub_stripe_checkout_with_invoice(
-      [{
-        price: 150,
-        description: "Early Bird - John Doe"
-      }],
+      [price: 150, description: "Early Bird - John Doe"],
       session_id: "stripe-session-id",
       session_url: "https://stripe-checkout-link.com/payment"
     )
 
     visit ticket_path(1)
 
-    expect(page).to have_content 'Buy Early Bird tickets'
+    assert_have_content page, 'Buy Early Bird tickets'
 
     fill_in 'checkout_tickets__name', with: 'John Doe'
     fill_in 'checkout_tickets__email', with: 'john@example.com'
@@ -23,6 +20,6 @@ RSpec.case "Buy tickets", type: :feature do
 
     click_button 'Pay'
 
-    expect(page.current_url).to eq("https://stripe-checkout-link.com/payment")
+    assert_eq page.current_url, "https://stripe-checkout-link.com/payment"
   end
 end
