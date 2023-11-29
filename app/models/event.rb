85b3ee206042c564c3c02a55bf377a60f9aca7e4
Genadi Samokovarov <gsamokovarov@gmail.com>
2023-11-29 12:00:09 +0200

    Seed data through the current event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,5 @@
 class Event < ApplicationRecord
+  has_many :ticket_types
+  has_many :order
+  has_many :tickets, through: :orders
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,14 @@
-event = Event.find_or_create_by!(name: "Balkan Ruby") do
+current_event = Event.find_or_create_by! name: "Balkan Ruby 2024" do
   _1.start_date = Date.new(2024, 4, 26)
   _1.end_date = Date.new(2024, 4, 27)
 end
 
-TicketType.find_or_create_by!(event: event, name: "Early Bird") do
+current_event.ticket_types.find_or_create_by! name: "Early Bird" do
   _1.price = 150
   _1.enabled = true
 end
 
+current_event.ticket_types.find_or_create_by! name: "Regular" do
 TicketType.find_or_create_by!(event: event, name: "Regular") do
   _1.price = 175
   _1.enabled = false
