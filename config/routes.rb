b32e2e05fd85403627c58bc7940b3bf10390f316
Genadi Samokovarov <gsamokovarov@gmail.com>
2023-12-01 08:27:22 +0200

    Fix the Tickets navigation link

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   root "home#show"
 
   resources :sponsors, only: [:index]
-  resources :tickets, only: [:index, :show]
+  resources :tickets, only: [:show]
   resources :speakers, only: [:index]
   resources :checkouts, only: [:index, :show, :create]
   resource :thanks, only: [:show]
