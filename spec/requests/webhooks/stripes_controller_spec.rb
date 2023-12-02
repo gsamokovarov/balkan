d04c8030dd6ffc3672ffdf820a32738aee7ff9c7
Svetlozar Mihailov <svetliomihailov@gmail.com>
2023-12-02 18:55:56 +0200

    Enable promo codes for the Stripe checkout session

--- spec/requests/webhooks/stripes_controller_spec.rb
+++ spec/requests/webhooks/stripes_controller_spec.rb
@@ -177,6 +177,113 @@ RSpec.case Webhooks::StripesController, type: :request do
     assert_eq order.tickets.count, 3
   end
 
+  test "finalizes order with promo code and sends ticket email on checkout.session.completed" do
+    stripe_completed_payload =
+      {
+        "id": "evt_1OI4uGCUZRkPCoUiF9Uko7Ej",
+        "object": "event",
+        "api_version": "2023-10-16",
+        "created": 1701330212,
+        "data": {
+          "object": {
+            "id": "cs_test_b14LXJrpxq5nATRjVFsAgnINrXd0CU6pM4sZyqSSplv8F42xIaIQHrHhgc",
+            "object": "checkout.session",
+            "after_expiration": nil,
+            "allow_promotion_codes": true,
+            "amount_subtotal": 30000,
+            "amount_total": 25500,
+            "automatic_tax": {"enabled": false, "status": nil},
+            "billing_address_collection": "required",
+            "cancel_url": "http://localhost:3000/",
+            "client_reference_id": nil,
+            "client_secret": "[FILTERED]",
+            "consent": nil,
+            "consent_collection": nil,
+            "created": 1701534163,
+            "currency": "eur",
+            "currency_conversion": nil,
+            "custom_fields": [],
+            "custom_text": { "shipping_address": nil, "submit": nil, "terms_of_service_acceptance": nil },
+            "customer": nil,
+            "customer_creation": "if_required",
+            "customer_details": {
+              "address": {
+                "city": "Sofia",
+                "country": "BG",
+                "line1": "Line 1",
+                "line2": nil,
+                "postal_code": "1000",
+                "state": nil
+              },
+              "email": "svetlozar.mihaylov@raketadesign.com",
+              "name": "SVETLOZAR MIHAYLOV",
+              "phone": nil,
+              "tax_exempt": "none",
+              "tax_ids": []
+            },
+            "customer_email": nil,
+            "expires_at": 1701620563,
+            "invoice": nil,
+            "invoice_creation": {
+              "enabled": false,
+              "invoice_data": {
+                "account_tax_ids": nil,
+                "custom_fields": nil,
+                "description": nil,
+                "footer": nil,
+                "metadata": {},
+                "rendering_options": nil
+              }
+            },
+            "livemode": false,
+            "locale": nil,
+            "metadata": {},
+            "mode": "payment",
+            "payment_intent": "pi_3OIvyJCUZRkPCoUi1wrnHgvT",
+            "payment_link": nil,
+            "payment_method_collection": "if_required",
+            "payment_method_configuration_details": nil,
+            "payment_method_options": {},
+            "payment_method_types": ["card"],
+            "payment_status": "paid",
+            "phone_number_collection": {"enabled": false},
+            "recovered_from": nil,
+            "setup_intent": nil,
+            "shipping_address_collection": nil,
+            "shipping_cost": nil,
+            "shipping_details": nil,
+            "shipping_options": [],
+            "status": "complete",
+            "submit_type": nil,
+            "subscription": nil,
+            "success_url": "http://localhost:3000/thanks",
+            "tax_id_collection": { "enabled": true },
+            "total_details": { "amount_discount": 4500, "amount_shipping": 0, "amount_tax": 0 },
+            "ui_mode": "hosted",
+            "url": nil}
+        },
+        "livemode": true,
+        "pending_webhooks": 1,
+        "request": { "id": nil, "idempotency_key": nil },
+        "type": "checkout.session.completed"
+      }
+
+    stripe_checkout_session_uid = stripe_completed_payload.dig(:data, :object, :id)
+    tickets_metadata = (1..2).map do
+      build_ticket_params(index: _1, price: 150)
+    end
+
+    order = create(:order, stripe_checkout_session_uid:,tickets_metadata:)
+
+    stripe_post stripe_completed_payload
+
+    assert_have_http_status response, :ok
+    assert_eq order.reload.completed_at?, true
+    assert_eq order.tickets.count, 2
+    assert_eq order.tickets.first.price, 127.5
+    assert_eq order.tickets.last.price, 127.5
+  end
+
   def build_ticket_params(index:, price:)
     {
       "name" => "John Doe #{index}",
