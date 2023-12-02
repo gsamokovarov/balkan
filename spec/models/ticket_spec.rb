90bd1a97c312e3ad894ffd3e8975c890cdb84914
Genadi Samokovarov <gsamokovarov@gmail.com>
2023-12-02 14:13:30 +0200

    Fix Ticket model tests

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -2,7 +2,8 @@ require "rails_helper"
 
 RSpec.case Ticket do
   test "#event_access_url" do
-    ticket = FactoryBot.create :ticket, :early_bird, name: "John Doe", email: "john@example.com"
+    order = FactoryBot.create :order, completed_at: Time.current
+    ticket = FactoryBot.create :ticket, :early_bird, order:, name: "John Doe", email: "john@example.com"
     token = ticket.generate_token_for :event_access
 
     assert_eq ticket.event_access_url, "http://example.com/tickets/#{token}"
