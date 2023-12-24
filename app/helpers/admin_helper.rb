966e3b293dc2d6fb6a76c929c7f36efc05151ab3
Genadi Samokovarov <gsamokovarov@gmail.com>
2023-12-24 10:25:20 +0200

    Iterate on orders redesign

---
+++ app/helpers/admin_helper.rb
@@ -0,0 +1,15 @@
+module AdminHelper
+  ADMIN_BADGE_TYPES = {
+    primary: "ring-green-600/20 bg-green-50 text-green-700",
+    danger: "ring-banitsa-500/20 bg-banitsa-50 text-banitsa-500"
+  }
+
+  def admin_badge(type, &)
+    classes = [
+      "inline-flex items-center rounded-md px-2 py-1 text-xs font-medium ring-1 ring-inset",
+      ADMIN_BADGE_TYPES.fetch(type)
+    ]
+
+    tag.span(class: classes, &)
+  end
+end
