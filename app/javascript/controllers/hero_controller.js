d88d18fdd3e61b5ec4dfddfdca5b8d7965ec1f05
Genadi Samokovarov <gsamokovarov@gmail.com>
2024-01-14 00:27:06 +0200

    Nudge CFP applications with a countdown
    And fix a bunch of spacing issues in the hero section along the way. A
    forgotten empty styled <h2> was an epic fail.

--- app/javascript/controllers/hero_controller.js
+++ app/javascript/controllers/hero_controller.js
@@ -6,4 +6,9 @@ export default class extends Controller {
     const ticketsElement = document.getElementById("tickets")
     if (ticketsElement) ticketsElement.scrollIntoView({ behavior: "smooth" })
   }
+
+  scrollToSpeakers() {
+    const ticketsElement = document.getElementById("speakers")
+    if (ticketsElement) ticketsElement.scrollIntoView({ behavior: "smooth" })
+  }
 }
--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module Balkan
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Sofia"
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
