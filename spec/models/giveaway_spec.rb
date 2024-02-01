b5a629780fd91a803cb29feb62acc21c0eaf3eb4
Genadi Samokovarov <gsamokovarov@gmail.com>
2024-02-01 11:49:44 +0200

    Allow custom reason when giving free tickets

--- spec/models/giveaway_spec.rb
+++ spec/models/giveaway_spec.rb
@@ -17,4 +17,19 @@ RSpec.case Giveaway do
     assert_eq order.completed_at.present?, true
     assert_eq order.amount, 0
   end
+
+  test "creates a free ticket with custom reason" do
+    event = create :event, :balkan2024
+    create(:ticket_type, :free, event:)
+
+    ticket = Giveaway.create_free_ticket event, name: "Great Person", email: "someone@great.com", shirt_size: "S",
+                                                reason: "Basic Sponsorship"
+
+    assert_eq ticket.name, "Great Person"
+    assert_eq ticket.email, "someone@great.com"
+    assert_eq ticket.shirt_size, "S"
+
+    order = ticket.order
+    assert_eq order.free_reason, "Basic Sponsorship"
+  end
 end
