e3513b5131d87e9b22d9ef60393abe080b47c3bd
Genadi Samokovarov <gsamokovarov@gmail.com>
2024-02-02 23:06:19 +0200

    Disable speaking applications after THE FINAL COUNTDOWN

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,6 +1,17 @@
 require "rails_helper"
 
-RSpec.case "Newsletter subscription", type: :feature do
+RSpec.case "Homepage", type: :feature do
+  test "after speaker applications end date" do
+    create :event, :balkan2024
+
+    travel_to Time.zone.local(2024, 2, 3, 0, 0) do
+      visit root_path
+
+      assert_not_have_content page, "Speaker applications close in"
+      assert_not_have_content page, "Become a speaker"
+    end
+  end
+
   test "subscribes user to a newsletter with email" do
     create :event, :balkan2024
 
