632d875d247202883bb0737e279c21077b12e149
Genadi Samokovarov <gsamokovarov@gmail.com>
2024-02-13 16:14:21 +0200

    Format the currency with standard Rails helpers
    It results in better presentation.

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -4,13 +4,13 @@ module Currency
   EUR_TO_BGN_RATE = "1.95583".to_d
 
   def format_money(eur, locale:)
-    case locale
-    when :bg, "bg"
-      "#{to_bgn(eur).round(2, :down)} лв."
-    else
-      "€#{eur.round(2, :down)}"
-    end
+    bulgarian = locale.to_sym == :bg
+    unit = bulgarian ? "лв." : "€"
+    format = bulgarian ? "%n %u" : "%u%n"
+    currency = bulgarian ? eur_to_bgn(eur) : eur
+
+    ActiveSupport::NumberHelper.number_to_currency currency, unit:, locale:, format:, round_mode: :down
   end
 
-  private def to_bgn(eur) = eur * EUR_TO_BGN_RATE
+  private def eur_to_bgn(eur) = eur * EUR_TO_BGN_RATE
 end
