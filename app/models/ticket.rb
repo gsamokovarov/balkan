a073eb84a7c9afa01b6d8ff849d965e931a5c562
Genadi Samokovarov <gsamokovarov@gmail.com>
2024-03-18 10:27:30 +0200

    Simplify Ticket#event_access_url and Ticket#qrcode

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -16,12 +16,6 @@ class Ticket < ApplicationRecord
 
   normalizes :email, with: -> { _1.downcase.strip }
 
-  def event_access_url
-    Link.ticket_url generate_token_for(:event_access)
-  end
-
-  def qrcode
-    event_access_token = generate_token_for(:event_access)
-    RQRCode::QRCode.new Link.ticket_url(event_access_token)
-  end
+  def event_access_url = Link.ticket_url generate_token_for(:event_access)
+  def qrcode = RQRCode::QRCode.new event_access_url
 end
