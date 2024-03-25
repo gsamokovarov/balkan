de2c30d50227c6160783872e48362b89e5a1fb43
Genadi Samokovarov <gsamokovarov@gmail.com>
2024-03-25 15:10:15 +0200

    Prepare the newsletter sending to Subscriber.with_ticket_holders

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -5,6 +5,22 @@ class Subscriber < ApplicationRecord
 
   generates_token_for :cancelation
 
+  def self.with_ticket_holders(event)
+    ticket_emails = event.tickets.select :email
+    tickets_without_id = event.tickets.select "NULL as id, tickets.email"
+
+    Subscriber.find_by_sql [<<-SQL, { event_id: event.id, tickets_without_id:, ticket_emails: }]
+      SELECT subscribers.id, subscribers.email
+        FROM subscribers
+       WHERE subscribers.event_id = :event_id
+         AND subscribers.email NOT IN (:ticket_emails)
+
+      UNION ALL
+
+      :tickets_without_id
+    SQL
+  end
+
   def cancel_url
     Link.subscriber_url generate_token_for(:cancelation)
   end
