2a0e710fec1c3f3372c81a5ef21e9f4dab08498b
Genadi Samokovarov <gsamokovarov@gmail.com>
2024-03-30 16:47:09 +0200

    Revert "Merge branch 'schedule'"
    This reverts commit 165d77962f07cb86a5ff5f0c8e6ec35fc47cee2c, reversing
    changes made to da22ee4e51817b88bfd9a4c43ff42784a541e326.

--- app/models/schedule.rb
+++
@@ -1,8 +0,0 @@
-class Schedule < ApplicationFrozenRecord
-  Timeslot = Data.define :time, :type, :talk_id do
-    def talk? = talk_id.present?
-    def talk = Talk.find(talk_id)
-  end
-
-  def timeslots = super.transform_values { |slots| slots.map { Timeslot.new(**_1) } }
-end
