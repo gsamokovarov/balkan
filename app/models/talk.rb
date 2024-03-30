af58f1c0010b6f038fd3daa4f20963e2c52948c2
Genadi Samokovarov <gsamokovarov@gmail.com>
2024-03-30 21:43:06 +0200

    Show full schedule names in a tooltip when truncated

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,9 +1,6 @@
 class Talk < ApplicationFrozenRecord
   Speaker = Data.define :name, :avatar, :github_url, :social_url, :about, :talk
 
-  def speakers
-    super.map { Speaker.new(**_1, talk: self) }
-  rescue StandardError
-    precondition_failure "Invalid speakers for talk ##{id}"
-  end
+  def speakers = super.map { Speaker.new(**_1, talk: self) }
+  def speaker_names = speakers.map(&:name).to_sentence
 end
