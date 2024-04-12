90d8954386bae529bbf16d7e8740cae6a14a91b8
Genadi Samokovarov <gsamokovarov@gmail.com>
2024-04-12 12:04:49 +0300

    Reapply "Merge branch 'simpler-error-pages'"
    This reverts commit aa746cb4a326e1ea4b9158186369d729de63d3d7.

--- public/assets/controllers/hero_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@hotwired/stimulus";
-
-// Connects to data-controller="hero"
-export default class extends Controller {
-  scrollToTickets() {
-    const ticketsElement = document.getElementById("tickets")
-    if (ticketsElement) ticketsElement.scrollIntoView({ behavior: "smooth" })
-  }
-}
