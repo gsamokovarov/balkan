1b21aeb9375612f4aef0689c6fb9dcc0e2522165
Genadi Samokovarov <gsamokovarov@gmail.com>
2024-04-23 11:58:01 +0300

    No more subscriptions tests
    We don't show them on the homepage

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -11,29 +11,4 @@ RSpec.case "Homepage", type: :feature do
       assert_not_have_content page, "Become a speaker"
     end
   end
-
-  test "subscribes user to a newsletter with email" do
-    create :event, :balkan2024
-
-    visit root_path
-
-    fill_in "subscriber_email", with: "genadi@hey.com"
-
-    click_button "Subscribe"
-
-    assert_have_content page, "We'll keep you up to date on genadi@hey.com"
-  end
-
-  test "unsubscribes user from a newsletter with a token" do
-    event = create :event, :balkan2024
-    subscriber = create(:subscriber, :genadi, event:)
-
-    visit subscriber.cancel_url
-
-    assert_have_content page, "You are about to unsubscribe genadi@hey.com"
-
-    click_button "Unsubscribe"
-
-    assert_have_content page, "You will no longer receive updates on genadi@hey.com"
-  end
 end
