d3b25fabf920fce3751f15f4816ed7a9dd41e859
Genadi Samokovarov <gsamokovarov@gmail.com>
2024-04-24 21:29:40 +0300

    Create free tickets in bulk

--- app/models/giveaway.rb
+++ app/models/giveaway.rb
@@ -8,10 +8,22 @@ module Giveaway
 
       order = Order.create! event:, name:, email:, free: true, free_reason: reason, completed_at: Time.current
       ticket = order.tickets.create! name:, email:, shirt_size:, ticket_type:, price: 0
-
       TicketMailer.welcome_email(ticket).deliver_later
-
       ticket
+    end
+  end
+
+  def create_free_tickets(event, tickets, reason: "Giveaway")
+    ApplicationRecord.transaction do
+      ticket_type = event.ticket_types.find_by! enabled: false, price: 0
+      precondition ticket_type.name == "Free"
+
+      name, email = tickets.first.values_at :name, :email
+
+      order = Order.create! event:, name:, email:, free: true, free_reason: reason, completed_at: Time.current
+      tickets = order.tickets.create! tickets.map { { **_1, ticket_type:, price: 0 } }
+      tickets.each { TicketMailer.welcome_email(_1).deliver_later }
+      tickets
     end
   end
 end
--- spec/models/giveaway_spec.rb
+++ spec/models/giveaway_spec.rb
@@ -34,4 +34,40 @@ RSpec.case Giveaway do
     order = ticket.order
     assert_eq order.free_reason, "Basic Sponsorship"
   end
+
+  test "creates a free tickets in bul" do
+    event = create :event, :balkan2024
+    create(:ticket_type, :free, event:)
+
+    tickets = Giveaway.create_free_tickets event, [
+      { name: "Genadi 1", email: "genadi1@hey.com", shirt_size: "S" },
+      { name: "Genadi 2", email: "genadi2@hey.com", shirt_size: "S" },
+      { name: "Genadi 3", email: "genadi3@hey.com", shirt_size: "S" },
+      { name: "Genadi 4", email: "genadi4@hey.com", shirt_size: "S" }
+    ]
+
+    assert_eq tickets[0].name, "Genadi 1"
+    assert_eq tickets[0].email, "genadi1@hey.com"
+    assert_eq tickets[0].shirt_size, "S"
+
+    assert_eq tickets[1].name, "Genadi 2"
+    assert_eq tickets[1].email, "genadi2@hey.com"
+    assert_eq tickets[1].shirt_size, "S"
+
+    assert_eq tickets[2].name, "Genadi 3"
+    assert_eq tickets[2].email, "genadi3@hey.com"
+    assert_eq tickets[2].shirt_size, "S"
+
+    assert_eq tickets[3].name, "Genadi 4"
+    assert_eq tickets[3].email, "genadi4@hey.com"
+    assert_eq tickets[3].shirt_size, "S"
+
+    order = tickets[0].order
+    assert_eq order.email, "genadi1@hey.com"
+    assert_eq order.name, "Genadi 1"
+    assert_eq order.free?, true
+    assert_eq order.free_reason, "Giveaway"
+    assert_eq order.completed_at.present?, true
+    assert_eq order.amount, 0
+  end
 end
