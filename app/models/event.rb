0c1af36512cb6441cc707154e0773dae6b7f8289
Genadi Samokovarov <gsamokovarov@gmail.com>
2024-10-06 16:13:01 +0300

    Bind static blogs, schedules and talks to events

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,11 +5,11 @@ class Event < ApplicationRecord
   has_many :subscribers
   has_one :invoice_sequence
 
-  def speakers = Talk.all.select(&:available).flat_map(&:speakers)
+  def speakers = Talk.where(event_id: id).select(&:available).flat_map(&:speakers)
   def sponsors = Sponsor.all
   def community_partners = CommunityPartner.all
-  def schedule = Schedule.find_by!(event_name: name)
-  def blog_posts = BlogPost.all.order(id: :desc)
+  def schedule = Schedule.find_by!(event_id: id)
+  def blog_posts = BlogPost.where(event_id: id).order(id: :desc)
 
   def speaker_applications_countdown = FinalCountdown.until speaker_applications_end_date
   def beginning_countdown = FinalCountdown.until start_date
