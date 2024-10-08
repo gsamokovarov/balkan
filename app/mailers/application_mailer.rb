986e0f7512191350c635ded5e02d6c5268b24d3e
Genadi Samokovarov <gsamokovarov@gmail.com>
2024-10-08 13:42:58 +0300

    Use genadi@balkanruby.com, even for banitsa event emails
    This email address is a verified sender. Use it for now.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,7 +2,7 @@ class ApplicationMailer < ActionMailer::Base
   include MailHelper
   helper MailHelper
 
-  default from: email_address_with_name("genadi@hey.com", "Genadi Samokovarov")
+  default from: email_address_with_name("genadi@balkanruby.com", "Genadi Samokovarov")
 
   layout "mailer"
 end
