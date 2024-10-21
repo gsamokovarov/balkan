f1c852a80f5b0981fcac8cae609cdc9a102fa3ac
Genadi Samokovarov <gsamokovarov@gmail.com>
2024-10-21 09:18:04 +0300

    Fix static talks links

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -1,9 +1,9 @@
 class TalksController < ApplicationController
   def show
-    @talk = Talk.find params[:id].to_i
+    @talk = StaticTalk.find params[:id].to_i
   end
 
   def thumbnail
-    @talk = Talk.find params[:id].to_i
+    @talk = StaticTalk.find params[:id].to_i
   end
 end
