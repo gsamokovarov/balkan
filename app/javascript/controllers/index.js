477514a71e6223857f6ea54e3c3d08671357041c
Genadi Samokovarov <gsamokovarov@gmail.com>
2024-11-17 19:39:45 +0200

    Fix Stimulus controller initialization

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,7 +6,7 @@ import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 const application = Application.start()
 
 // Configure Stimulus development experience
-application.debug = document.head.queryselector(`meta[name="env"]`)?.content === "development"
+application.debug = document.head.querySelector(`meta[name="env"]`)?.content === "development"
 window.Stimulus   = application
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
