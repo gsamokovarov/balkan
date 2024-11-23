127253846c7b68c37943c08cc17e9fbeee48511c
Genadi Samokovarov <gsamokovarov@gmail.com>
2024-11-23 22:23:49 +0200

    Refactor form building implementation
    DRY-up the field classes definition and reuse the admin buttons, which
    means no more full-width buttons for them forms only.

--- app/models/admin/form_builder.rb
+++ app/models/admin/form_builder.rb
@@ -1,48 +1,33 @@
 class Admin::FormBuilder < ActionView::Helpers::FormBuilder
   FIELD_CLASSES = {
-    base: "block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6",
+    base: [
+      "block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset focus:ring-2 focus:ring-inset",
+      "sm:text-sm sm:leading-6"
+    ],
     error: "text-red-900 ring-red-300 placeholder:text-red-300 focus:ring-red-500",
     valid: "text-gray-900 ring-gray-300 placeholder:text-gray-400 focus:ring-indigo-600"
   }
 
   def text_input(method, **)
-    classes = [
-      FIELD_CLASSES[:base],
-      object.errors[method].any? && FIELD_CLASSES[:error],
-      object.errors[method].none? && FIELD_CLASSES[:valid]
-    ]
-
     input_for method do
-      @template.concat text_field(method, class: classes, **)
+      @template.concat text_field(method, class: field_classes(method), **)
     end
   end
 
   def text_area_input(method, **)
-    classes = [
-      FIELD_CLASSES[:base],
-      object.errors[method].any? && FIELD_CLASSES[:error],
-      object.errors[method].none? && FIELD_CLASSES[:valid]
-    ]
-
     input_for method do
-      @template.concat text_area(method, class: classes, **)
+      @template.concat text_area(method, class: field_classes(method), **)
     end
   end
 
   def password_input(method, **)
-    classes = [
-      FIELD_CLASSES[:base],
-      object.errors[method].any? && FIELD_CLASSES[:error],
-      object.errors[method].none? && FIELD_CLASSES[:valid]
-    ]
-
     input_for method do
-      @template.concat password_field(method, class: classes, **)
+      @template.concat password_field(method, class: field_classes(method), **)
     end
   end
 
   def file_input(method, **)
-    field_classes = [
+    file_classes = [
       "block w-full rounded-md p-2 text-sm text-gray-500 shadow-sm ring-1 ring-inset ring-gray-300",
       "focus:ring-indigo-600"
     ]
@@ -56,19 +41,14 @@ class Admin::FormBuilder < ActionView::Helpers::FormBuilder
       if attachment.attached?
         @template.concat @template.image_tag(attachment, class: image_preview_classes)
       end
-      @template.concat file_field(method, class: field_classes, **)
+      @template.concat file_field(method, class: file_classes, **)
     end
   end
 
   def submit(value = nil, **)
     value ||= object.persisted? ? "Update" : "Save"
-    classes = [
-      "inline-block rounded-md px-3 py-2 w-full text-center text-sm font-semibold shadow-sm",
-      "focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2",
-      "bg-indigo-600 text-white hover:bg-indigo-500 focus-visible:outline-indigo-600 ring-indigo-300"
-    ]
 
-    @template.tag.button(value, class: classes, **)
+    @template.admin_button(:primary, type: :submit) { value }
   end
 
   private
@@ -81,5 +61,13 @@ class Admin::FormBuilder < ActionView::Helpers::FormBuilder
         @template.concat @template.tag.p object.errors[method].first, class: "mt-2 text-sm text-red-600"
       end
     end
+  end
+
+  def field_classes(method)
+    [
+      *FIELD_CLASSES[:base],
+      object.errors[method].any? && FIELD_CLASSES[:error],
+      object.errors[method].none? && FIELD_CLASSES[:valid]
+    ]
   end
 end
