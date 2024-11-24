ba3cb1e0826d8f677430ab0b917f44850f1c3018
Genadi Samokovarov <gsamokovarov@gmail.com>
2024-11-24 13:11:13 +0200

    Don't cache ApplicationRecord.total_count
    It becomes one per class, we don't want that. :)

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,5 +2,5 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   def self.page(number, per_page: 50) = limit(per_page).offset(per_page * [number.to_i - 1, 0].max)
-  def self.total_count = @total_count ||= offset(nil).limit(nil).count
+  def self.total_count = offset(nil).limit(nil).count
 end
