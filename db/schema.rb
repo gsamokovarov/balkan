1a770b2c2d5696939a8467b5d63673b0118d698f
Genadi Samokovarov <gsamokovarov@gmail.com>
2024-11-25 11:38:54 +0200

    schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_25_090324) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_25_093108) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -79,6 +79,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_25_090324) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "role", default: "Speaker"
+    t.integer "status", default: 0, null: false
     t.index ["event_id"], name: "index_lineup_members_on_event_id"
     t.index ["speaker_id"], name: "index_lineup_members_on_speaker_id"
     t.index ["talk_id"], name: "index_lineup_members_on_talk_id"
