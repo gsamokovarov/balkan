fe3736cd03795a083c710f8b053b1037e6cfbdc0
Genadi Samokovarov <gsamokovarov@gmail.com>
2024-12-01 14:41:56 +0200

    schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_25_093108) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_01_123821) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_25_093108) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "community_partners", force: :cascade do |t|
+    t.integer "event_id", null: false
+    t.string "name", null: false
+    t.string "url", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_community_partners_on_event_id"
   end
 
   create_table "events", force: :cascade do |t|
@@ -161,6 +170,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_25_093108) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "community_partners", "events"
   add_foreign_key "events", "invoice_sequences"
   add_foreign_key "invoices", "invoice_sequences"
   add_foreign_key "invoices", "orders"
