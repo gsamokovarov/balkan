3d4ae85dbcf6a0dda687ae811ccc74d58e038fe3
Genadi Samokovarov <gsamokovarov@gmail.com>
2024-12-13 14:14:58 +0200

    Treat Schedule#published_at as Schedule#published boolean
    This introduces ApplicationRecord.time_as_boolean that generates boolean
    attributes for time fields. By default we follow #{attribute}_at for the
    timestamp fields but this can be configured with the field: keyword
    argument.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,6 +1,13 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  def self.page(number, per_page: 50) = limit(per_page).offset(per_page * [number.to_i - 1, 0].max)
-  def self.total_count = offset(nil).limit(nil).count
+  class << self
+    def page(number, per_page: 50) = limit(per_page).offset(per_page * [number.to_i - 1, 0].max)
+    def total_count = offset(nil).limit(nil).count
+
+    def time_as_boolean(attribute, field: "#{attribute}_at")
+      define_method attribute, -> { read_attribute(field).presence }
+      define_method "#{attribute}=", -> value { write_attribute(field, value.presence ? Time.current : nil) }
+    end
+  end
 end
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,5 +2,7 @@ class Schedule < ApplicationRecord
   belongs_to :event
   has_many :slots, -> { order(:date, :time) }, class_name: "ScheduleSlot"
 
+  time_as_boolean :published
+
   def slots_for(date) = slots.select { _1.date == date }
 end
