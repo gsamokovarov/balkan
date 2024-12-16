3f1caabed9347fbacc4b49f8f099cbcdeaa33d9a
Genadi Samokovarov <gsamokovarov@gmail.com>
2024-12-16 12:13:50 +0200

    Subscription welcoming mention the event name

--- app/mailers/subscriber_mailer.rb
+++ app/mailers/subscriber_mailer.rb
@@ -2,7 +2,7 @@ class SubscriberMailer < ApplicationMailer
   def welcome_email(subscriber)
     @subscriber = subscriber
 
-    mail to: @subscriber.email, subject: "Welcome to Balkan Ruby newsletter!"
+    mail to: @subscriber.email, subject: "Welcome to #{@subscriber.event.name} newsletter!"
   end
 
   def newsletter_1_email(subscriber)
