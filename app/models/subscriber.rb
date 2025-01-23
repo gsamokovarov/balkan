73ea7282cb7ed813a6fb8219f98aaaa9f9a9abef
Genadi Samokovarov <gsamokovarov@gmail.com>
2025-01-23 10:16:05 +0200

    Support multiple events when sending subscriptions emails

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -5,22 +5,20 @@ class Subscriber < ApplicationRecord
 
   generates_token_for :cancelation
 
-  class << self
-    def including_ticket_holders(event)
-      ticket_emails = event.tickets.select :email
-      tickets_query = event.tickets.select "NULL as id, tickets.email"
+  def self.including_ticket_holders(*events)
+    ticket_emails = Ticket.joins(:order).where(order: { event: events }).select :email
+    tickets_query = Ticket.joins(:order).where(order: { event: events }).select "NULL as id, tickets.email"
 
-      Subscriber.find_by_sql [<<-SQL, { event_id: event.id, tickets_query:, ticket_emails: }]
+    Subscriber.find_by_sql [<<-SQL, event_ids: events.pluck(:id), tickets_query:, ticket_emails:]
       SELECT subscribers.id, subscribers.email
         FROM subscribers
-       WHERE subscribers.event_id = :event_id
+       WHERE subscribers.event_id IN (:event_ids)
          AND subscribers.email NOT IN (:ticket_emails)
 
-      UNION ALL
+      UNION
 
       :tickets_query
-      SQL
-    end
+    SQL
   end
 
   def cancel_url = Link.subscriber_url generate_token_for(:cancelation)
