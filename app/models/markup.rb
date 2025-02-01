38b7c9392d2fd2cfd1471e96173bd4575788f9af
Genadi Samokovarov <gsamokovarov@gmail.com>
2025-02-01 15:59:48 +0200

    Render markdown with inlined images straight from Markup.render_html

--- app/models/markup.rb
+++ app/models/markup.rb
@@ -5,6 +5,8 @@ require "redcarpet/render_strip"
 module Markup
   extend self
 
+  INLINE_IMAGE_PLACEHOLDER = /\[(\d+)\]/
+
   class PlainTextRenderer < Redcarpet::Render::StripDown
     def paragraph(text) = "#{text}\n\n"
     def header(text, _header_level) = "#{text}\n\n"
@@ -12,10 +14,18 @@ module Markup
 
   class HTMLRenderer < Redcarpet::Render::HTML
     def link(link, title, content) = %(<a href="#{link}" title="#{title}" class="link-primary">#{content}</a>)
+    def image(link, title, alt) = %(<img src="#{link}" title="#{title}" alt="#{alt}" class="border-2 border-black rounded-md">)
   end
 
-  def render_html(content)
+  def render_html(content, inline_images: nil)
     @html_renderer ||= Redcarpet::Markdown.new HTMLRenderer, autolink: true, tables: true, strikethrough: true
+
+    if inline_images
+      content.gsub! INLINE_IMAGE_PLACEHOLDER do
+        "![image #{it[1]}](#{Link.url_for inline_images[it[1].to_i - 1]})"
+      end
+    end
+
     @html_renderer.render content
   end
 
