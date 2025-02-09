e8fd131c3ec4d7f2fdfcd0aa29e759f2ed79209c
Genadi Samokovarov <gsamokovarov@gmail.com>
2025-02-09 11:29:43 +0200

    schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_08_100102) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_09_083615) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,18 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_08_100102) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "blog_posts", force: :cascade do |t|
+    t.integer "event_id", null: false
+    t.integer "author_id", null: false
+    t.date "date"
+    t.string "title"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_blog_posts_on_author_id"
+    t.index ["event_id"], name: "index_blog_posts_on_event_id"
   end
 
   create_table "community_partners", force: :cascade do |t|
@@ -271,6 +283,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_08_100102) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "blog_posts", "events"
+  add_foreign_key "blog_posts", "users", column: "author_id"
   add_foreign_key "community_partners", "events"
   add_foreign_key "embeddings", "events"
   add_foreign_key "events", "invoice_sequences"
