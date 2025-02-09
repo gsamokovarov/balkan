73ef98bdc36b8bb40009c34b8a491c32997df12c
Genadi Samokovarov <gsamokovarov@gmail.com>
2025-02-09 11:42:39 +0200

    Setup name and bio for users in the admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
+  has_many :blogs, dependent: :destroy
 
   normalizes :email, with: -> { it.strip.downcase }
+  validates :name, presence: true
 end
