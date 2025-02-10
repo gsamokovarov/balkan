a6cf0cefe1ddf7b8c0bde79d917958e47b75585e
Genadi Samokovarov <gsamokovarov@gmail.com>
2025-02-10 20:28:06 +0200

    schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_09_093339) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_10_182724) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -47,6 +47,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_09_093339) do
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "published_at"
     t.index ["author_id"], name: "index_blog_posts_on_author_id"
     t.index ["event_id"], name: "index_blog_posts_on_event_id"
   end
