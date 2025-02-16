263c540029ebd7876c8770740d2fc48eb53e97aa
Genadi Samokovarov <gsamokovarov@gmail.com>
2025-02-16 19:23:41 +0200

    Fix sponsorship rendering N+1

--- app/models/sponsorship.rb
+++ app/models/sponsorship.rb
@@ -1,6 +1,6 @@
 class Sponsorship < ApplicationRecord
   belongs_to :event
-  belongs_to :sponsor
+  belongs_to :sponsor, -> { with_attached_logo }
   belongs_to :variant, class_name: "SponsorshipVariant"
   has_one :package, through: :variant
 end
