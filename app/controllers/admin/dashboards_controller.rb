d721ffd42cd67af891dec5ce098054f5f3deb935
Genadi Samokovarov <gsamokovarov@gmail.com>
2025-02-21 20:10:28 +0200

    Fix orders pagination

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -1,9 +1,9 @@
 class Admin::DashboardsController < Admin::ApplicationController
   def show
-    @orders = Current.event.orders.completed.includes(tickets: :ticket_type).order("completed_at DESC")
+    @orders = Current.event.orders.completed.includes(tickets: :ticket_type).order(completed_at: :desc)
   end
 
   def orders
-    @orders = Current.event.orders.completed.order "completed_at DESC"
+    @orders = scope Current.event.orders.completed.order(completed_at: :desc)
   end
 end
