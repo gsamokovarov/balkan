195214e06dc7b68051081afa2a0ef45b50ad3ee2
Genadi Samokovarov <gsamokovarov@gmail.com>
2025-02-25 17:36:35 +0200

    Show fiscal sponsors with bigger logos

--- app/models/sponsorship.rb
+++ app/models/sponsorship.rb
@@ -3,4 +3,6 @@ class Sponsorship < ApplicationRecord
   belongs_to :sponsor, -> { with_attached_logo }
   belongs_to :variant, class_name: "SponsorshipVariant"
   has_one :package, through: :variant
+
+  def fiscal? = price_paid.positive?
 end
