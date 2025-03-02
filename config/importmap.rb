294e21a7ab9fa32df38a2a053d23e4b4038da472
Genadi Samokovarov <gsamokovarov@gmail.com>
2025-03-02 20:49:50 +0200

    Try a screenshot with html2canvas when in need

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,4 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "reveal.js", to: "https://ga.jspm.io/npm:reveal.js@5.0.5/dist/reveal.esm.js"
+pin "html2canvas", to: "https://ga.jspm.io/npm:html2canvas@1.4.1/dist/html2canvas.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
