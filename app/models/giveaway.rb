841b1cf6c3e85f4abdcd3f0dca45010e0adc6d94
Genadi Samokovarov <gsamokovarov@gmail.com>
2025-03-13 20:23:01 +0200

    Simplify Giveaway
    We have an interface for creating the giveaway no, I don't need a
    convenience method for a single ticket giveaway.

--- app/models/giveaway.rb
+++ app/models/giveaway.rb
@@ -1,19 +1,6 @@
 module Giveaway
   extend self
 
-  def create_free_ticket(event, name:, email:, shirt_size:, reason: "Giveaway")
-    ticket =
-      ApplicationRecord.transaction do
-        ticket_type = event.ticket_types.find_by! name: "Free", enabled: false, price: 0
-
-        order = Order.create! event:, name:, email:, free: true, free_reason: reason, completed_at: Time.current
-        order.tickets.create! name:, email:, shirt_size:, ticket_type:, price: 0
-      end
-
-    TicketMailer.welcome_email(ticket).deliver_later
-    ticket
-  end
-
   def create_free_tickets(event, tickets, reason: "Giveaway")
     tickets =
       ApplicationRecord.transaction do
--- spec/models/giveaway_spec.rb
+++ spec/models/giveaway_spec.rb
@@ -1,41 +1,7 @@
 require "rails_helper"
 
 RSpec.case Giveaway do
-  test "creates a free ticket" do
-    event = create :event, :balkan2024
-    create(:ticket_type, :free, event:)
-
-    ticket = Giveaway.create_free_ticket event, name: "Genadi", email: "genadi@hey.com", shirt_size: "S"
-
-    assert_eq ticket.name, "Genadi"
-    assert_eq ticket.email, "genadi@hey.com"
-    assert_eq ticket.shirt_size, "S"
-
-    order = ticket.order
-    assert_eq order.email, "genadi@hey.com"
-    assert_eq order.name, "Genadi"
-    assert_eq order.free?, true
-    assert_eq order.free_reason, "Giveaway"
-    assert_eq order.completed_at.present?, true
-    assert_eq order.amount, 0
-  end
-
-  test "creates a free ticket with custom reason" do
-    event = create :event, :balkan2024
-    create(:ticket_type, :free, event:)
-
-    ticket = Giveaway.create_free_ticket event, name: "Great Person", email: "someone@great.com", shirt_size: "S",
-                                                reason: "Basic Sponsorship"
-
-    assert_eq ticket.name, "Great Person"
-    assert_eq ticket.email, "someone@great.com"
-    assert_eq ticket.shirt_size, "S"
-
-    order = ticket.order
-    assert_eq order.free_reason, "Basic Sponsorship"
-  end
-
-  test "creates a free tickets in bul" do
+  test "creates multiple free tickets with a reason" do
     event = create :event, :balkan2024
     create(:ticket_type, :free, event:)
 
@@ -44,7 +10,7 @@ RSpec.case Giveaway do
       { name: "Genadi 2", email: "genadi2@hey.com", shirt_size: "S" },
       { name: "Genadi 3", email: "genadi3@hey.com", shirt_size: "S" },
       { name: "Genadi 4", email: "genadi4@hey.com", shirt_size: "S" },
-    ]
+    ], reason: "Testing giveaway"
 
     assert_eq tickets[0].name, "Genadi 1"
     assert_eq tickets[0].email, "genadi1@hey.com"
@@ -66,7 +32,7 @@ RSpec.case Giveaway do
     assert_eq order.email, "genadi1@hey.com"
     assert_eq order.name, "Genadi 1"
     assert_eq order.free?, true
-    assert_eq order.free_reason, "Giveaway"
+    assert_eq order.free_reason, "Testing giveaway"
     assert_eq order.completed_at.present?, true
     assert_eq order.amount, 0
   end
