469219d2ca4e8e14daa1515b5f316a7c6951e809
Genadi Samokovarov <gsamokovarov@gmail.com>
2025-03-13 19:35:56 +0200

    Don't send emails in transactions
    They need to serialize and de-serialize the tickets and they won't be
    found in the database. Send the emails AFTER the free tickets are
    created.

--- app/models/giveaway.rb
+++ app/models/giveaway.rb
@@ -14,16 +14,18 @@ module Giveaway
   end
 
   def create_free_tickets(event, tickets, reason: "Giveaway")
-    ApplicationRecord.transaction do
-      ticket_type = event.ticket_types.find_by! enabled: false, price: 0
-      precondition ticket_type.name == "Free"
+    tickets =
+      ApplicationRecord.transaction do
+        ticket_type = event.ticket_types.find_by! enabled: false, price: 0
+        precondition ticket_type.name == "Free"
 
-      name, email = tickets.first.values_at :name, :email
+        name, email = tickets.first.values_at :name, :email
 
-      order = Order.create! event:, name:, email:, free: true, free_reason: reason, completed_at: Time.current
-      tickets = order.tickets.create!(tickets.map { { **it, ticket_type:, price: 0 } })
-      tickets.each { TicketMailer.welcome_email(it).deliver_later }
-      tickets
-    end
+        order = Order.create! event:, name:, email:, free: true, free_reason: reason, completed_at: Time.current
+        order.tickets.create!(tickets.map { { **it, ticket_type:, price: 0 } })
+      end
+
+    tickets.each { TicketMailer.welcome_email(it).deliver_later }
+    tickets
   end
 end
