8e630a06ec26e13a18d378b9e3ce1ea08f06e6ec
Genadi Samokovarov <gsamokovarov@gmail.com>
2025-04-11 15:11:43 +0300

    Attach images for the sponsorships themselves
    I'll use the during the conference presentation.

--- app/models/sponsorship.rb
+++ app/models/sponsorship.rb
@@ -3,6 +3,7 @@ class Sponsorship < ApplicationRecord
   belongs_to :sponsor, -> { with_attached_logo }
   belongs_to :variant, class_name: "SponsorshipVariant"
   has_one :package, through: :variant
+  has_one_attached :ogp_image
 
   def fiscal? = price_paid.positive?
 end
