89befedb2806f70aa1852d3ff7f86280a7f02d65
Genadi Samokovarov <gsamokovarov@gmail.com>
2025-04-25 10:52:32 +0300

    SELL THEM TICKETS

--- app/models/checkout.rb
+++ app/models/checkout.rb
@@ -5,7 +5,6 @@ module Checkout
     ticket_type = TicketType.enabled.find params.fetch(:ticket_type_id)
 
     precondition params[:tickets].present?, "At least 1 ticket needs to be created"
-    precondition ticket_type.event.upcoming?, "Event has started or is in the past"
 
     ApplicationRecord.transaction do
       order = Order.create! event: ticket_type.event
