9b353462da0c38bac2c237bea0266e184265e49c
Genadi Samokovarov <gsamokovarov@gmail.com>
2025-05-25 11:13:58 +0300

    Attach videos to the talks themselves
    Folks can watch the talk video directly on our website.

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -5,4 +5,16 @@ class Talk < ApplicationRecord
   validates :name, presence: true
 
   def speaker_names = speakers.map(&:name).to_sentence
+
+  def video_embed_url
+    return nil unless video_url.present?
+
+    video_id =
+      case video_url
+      when /youtube\.com.*[?&]v=([^&]+)/, %r{youtube\.com/embed/([^?]+)}, %r{youtu\.be/([^?]+)}
+        Regexp.last_match 1
+      end
+
+    "https://www.youtube.com/embed/#{video_id}" if video_id
+  end
 end
