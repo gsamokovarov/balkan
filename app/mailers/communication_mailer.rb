8e3371a2d646b926948de87f8be833562befc931
Genadi Samokovarov <gsamokovarov@gmail.com>
2025-12-28 12:38:32 +0200

    WIP

--- app/mailers/communication_mailer.rb
+++ app/mailers/communication_mailer.rb
@@ -1,11 +1,9 @@
 class CommunicationMailer < ApplicationMailer
-  def campaign_email(communication, recipient_email)
+  def template_email(communication, recipient_email)
     @communication = communication
-    @recipient_email = recipient_email
-    @rendered = communication.render_for recipient_email
+    communication.render_for(recipient_email) => { subject:, body: }
+    @body = body
 
-    mail to: recipient_email, subject: @rendered[:subject] do |format|
-      format.html { render inline: @rendered[:body] }
-    end
+    mail to: recipient_email, subject:
   end
 end
