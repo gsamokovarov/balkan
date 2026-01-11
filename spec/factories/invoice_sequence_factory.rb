5f9cae9723c99165a5ee225979d3eb761a4d764a
Genadi Samokovarov <gsamokovarov@gmail.com>
2026-01-11 18:53:15 +0200

    Display the invoice sequence name in the admin

--- spec/factories/invoice_sequence_factory.rb
+++ spec/factories/invoice_sequence_factory.rb
@@ -1,6 +1,9 @@
 FactoryBot.define do
   factory :invoice_sequence do
+    sequence(:name) { |n| "Sequence #{n}" }
+
     trait :balkan2024 do
+      name { "Balkan Ruby 2024" }
       initial_number { 10_001_049 }
     end
   end
