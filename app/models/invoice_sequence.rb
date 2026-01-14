1b686982fcbc99fe63c8467b440423643de0e3f6
Genadi Samokovarov <gsamokovarov@gmail.com>
2026-01-14 18:02:17 +0200

    Show invoice sequence names with their initial number

--- app/models/invoice_sequence.rb
+++ app/models/invoice_sequence.rb
@@ -1,7 +1,7 @@
 class InvoiceSequence < ApplicationRecord
   has_many :invoices
 
-  def name = super || initial_number
+  def display_name = name ? "#{name} (#{initial_number})" : initial_number
 
   def next_invoice_number
     last_invoice_number = invoices.maximum :number
