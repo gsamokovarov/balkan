74bbc9e48b497305242f0c0feb98ec60068b6549
Genadi Samokovarov <gsamokovarov@gmail.com>
2026-01-17 20:13:39 +0200

    Fix the invoice sequence prefix presentation

--- app/models/invoice_sequence.rb
+++ app/models/invoice_sequence.rb
@@ -1,7 +1,7 @@
 class InvoiceSequence < ApplicationRecord
   has_many :invoices
 
-  def display_name = name ? "#{name} (#{format('%010d', initial_number)}" : format("%010d", initial_number)
+  def display_name = name ? "#{name} (#{format('%010d', initial_number)})" : format("%010d", initial_number)
 
   def next_invoice_number
     last_invoice_number = invoices.maximum :number
