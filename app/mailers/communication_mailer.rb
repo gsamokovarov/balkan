5750e0a7a8147fc8bcdfd8886dd5abf0b1b17ec5
Genadi Samokovarov <gsamokovarov@gmail.com>
2026-02-05 15:50:53 +0200

    Find the subscriber during comms so we can properly show unsubscribe links
    This is fine in SQLite. N+1s are method calls. But maybe we have a
    better model somewhere.

--- app/mailers/communication_mailer.rb
+++ app/mailers/communication_mailer.rb
@@ -1,8 +1,7 @@
 class CommunicationMailer < ApplicationMailer
   def template_email(communication, recipient)
     communication.interpolate_for(recipient.email) => { subject:, body: }
-    @communication = communication
-    @recipient = recipient
+    @subscriber = communication.to_subscribers? && Subscriber.find_by(email: recipient.email)
     @body = body
 
     mail to: recipient.email, subject:
