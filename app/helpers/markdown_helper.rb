d374c733305f880c6c6fd2e8fc913fc0691efd58
Genadi Samokovarov <gsamokovarov@gmail.com>
2026-03-21 23:06:32 +0200

    Introduce prawn PDF rendering utilities in MarkdownHelper

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -27,8 +27,8 @@ module MarkdownHelper
     class HTMLRenderer < Redcarpet::Render::HTML
       def link(link, title, content) = %(<a href="#{link}" title="#{title}" class="link-primary">#{content}</a>)
 
-      def block_code(code, language)
-        %(<pre class="overflow-x-auto"><code>#{ERB::Util.html_escape(code)}</code></pre>)
+      def block_code(code, _language)
+        %(<pre class="overflow-x-auto"><code>#{ERB::Util.html_escape code}</code></pre>)
       end
 
       def image(link, title, alt)
@@ -38,6 +38,57 @@ module MarkdownHelper
 
         %(<img src="#{link}" title="#{title}" alt="#{alt}" class="max-w-full h-auto border-2 border-black rounded-md">)
       end
+    end
+
+    class PDFRenderer < Redcarpet::Render::Base
+      def initialize(document)
+        super()
+        @pdf = document
+        @list_counter = 0
+      end
+
+      def paragraph(text)
+        @pdf.text text, size: 11, inline_format: true, leading: 4
+        @pdf.move_down 8
+        ""
+      end
+
+      def header(text, level)
+        sizes = { 1 => 22, 2 => 16, 3 => 13 }
+        @pdf.move_down 8
+        @pdf.text text, size: sizes.fetch(level, 11), style: :bold, inline_format: true
+        @pdf.move_down level == 1 ? 8 : 4
+        ""
+      end
+
+      def list_item(text, list_type)
+        prefix = list_type == :ordered ? "#{@list_counter += 1}." : "\u2022"
+        @pdf.indent 20 do
+          @pdf.text "#{prefix} #{text.strip}", size: 11, inline_format: true, leading: 4
+        end
+        ""
+      end
+
+      def list(_content, _list_type)
+        @list_counter = 0
+        @pdf.move_down 8
+        ""
+      end
+
+      def hrule
+        @pdf.start_new_page
+        ""
+      end
+
+      def double_emphasis(text) = "<b>#{text}</b>"
+      def emphasis(text) = "<i>#{text}</i>"
+      def normal_text(text) = text
+      def codespan(code) = code
+      def linebreak = "\n"
+      def link(_link, _title, content) = content
+      def autolink(link, _link_type) = link
+      def doc_header = ""
+      def doc_footer = ""
     end
 
     def render_html(content, images: nil)
@@ -56,8 +107,16 @@ module MarkdownHelper
       sanitizer = Rails::HTML::FullSanitizer.new
       sanitizer.sanitize @plain_renderer.render(content).strip
     end
+
+    def render_pdf(content, document:)
+      renderer = PDFRenderer.new document
+      md = Redcarpet::Markdown.new(renderer, **DEFAULT_EXTENSIONS)
+      md.render content
+      nil
+    end
   end
 
   def render_markdown(...) = Markup.render_html(...).html_safe
   def render_plain(...) = Markup.render_plain(...)
+  def render_pdf(...) = Markup.render_pdf(...)
 end
