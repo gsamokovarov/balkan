e6af6b2398fd2ef0e3f7981272de9bc99ff7345d
Genadi Samokovarov <gsamokovarov@gmail.com>
2026-03-31 10:57:04 +0300

    schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_03_27_120249) do
+ActiveRecord::Schema[8.0].define(version: 2026_03_31_074856) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -108,6 +108,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_03_27_120249) do
     t.text "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "language", default: "en", null: false
     t.index ["event_id"], name: "index_contract_templates_on_event_id"
   end
 
