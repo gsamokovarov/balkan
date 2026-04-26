8edef2f4258bfdce80ffb4007b81bf79f6ade857
Genadi Samokovarov <gsamokovarov@gmail.com>
2026-04-26 17:49:35 +0300

    Add an option to attach sponsor logos to the email communication

--- app/mailers/communication_mailer.rb
+++ app/mailers/communication_mailer.rb
@@ -3,6 +3,14 @@ class CommunicationMailer < ApplicationMailer
     communication.interpolate_for(recipient.email) => { subject:, body: }
     @subscriber = communication.to_subscribers? && Subscriber.find_by(email: recipient.email)
     @body = body
+    @sponsor_attachments =
+      if communication.with_sponsors?
+        communication.event.sponsors.filter_map do |sponsorship|
+          filename = "sponsor-#{sponsorship.id}-#{sponsorship.sponsor.logo.filename}"
+          attachments.inline[filename] = sponsorship.sponsor.logo.download
+          { name: sponsorship.sponsor.name, filename: }
+        end
+      end
 
     mail to: recipient.email, subject:
   end
