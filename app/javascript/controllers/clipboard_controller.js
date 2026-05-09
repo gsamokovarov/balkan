0b15be6ade085b69cb6cd67c9778dcc1c2b6268c
Genadi Samokovarov <gsamokovarov@gmail.com>
2026-05-09 17:15:55 +0300

    Do a simpler copy interaction

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -2,25 +2,16 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="clipboard"
 export default class extends Controller {
-  static targets = ["source", "indicator"]
+  static targets = ["source"]
 
-  copy() {
+  copy(event) {
     event.preventDefault()
 
-    const copiedValue = this.sourceTarget.textContent.trim()
+    navigator.clipboard.writeText(this.sourceTarget.textContent.trim())
 
-    navigator.clipboard.writeText(copiedValue)
-    this.indicate(copiedValue)
-  }
-
-  indicate(copiedValue) {
-    const copiedContent = document.createElement("b")
-    copiedContent.textContent = copiedValue
-
-    const previousHTML = this.indicatorTarget.innerHTML
-    this.indicatorTarget.innerHTML = `Copied ${copiedContent.outerHTML}`
-    setTimeout(() => {
-      this.indicatorTarget.innerHTML = previousHTML
-    }, 5000)
+    const button = event.currentTarget
+    const original = button.textContent
+    button.textContent = "Copied!"
+    setTimeout(() => { button.textContent = original }, 2000)
   }
 }
