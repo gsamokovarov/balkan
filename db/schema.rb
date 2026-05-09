f44de7fbedc8b958b6688a1c9ed6815fa7127ddb
Genadi Samokovarov <gsamokovarov@gmail.com>
2026-05-09 16:35:34 +0300

    schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_05_09_120001) do
+ActiveRecord::Schema[8.0].define(version: 2026_05_09_130000) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -455,7 +455,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_05_09_120001) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
-    t.string "password_digest", null: false
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name", null: false
