cfee65b6605175994c848c427fb140cf8864d285
Genadi Samokovarov <gsamokovarov@gmail.com>
2026-06-05 10:20:09 +0300

    Introduce a full-screen gallery interaction
    This will be useful on mobile, but even the desktop is good.

--- app/javascript/controllers/gallery_controller.js
+++ app/javascript/controllers/gallery_controller.js
@@ -1,8 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="gallery"
+//
+// Rotates through a list of image URLs while keeping only a single <img> per
+// view in the DOM, so 20-50+ photos stay light. Tapping the gallery opens a
+// full-screen, modal-like overlay (no native fullscreen API) that reads well on
+// mobile. The next image is preloaded so each swap is instant.
 export default class extends Controller {
-  static targets = ["image", "counter"]
+  static targets = ["image", "counter", "overlay"]
   static values = {
     urls: Array,
     interval: { type: Number, default: 5000 },
@@ -16,6 +21,18 @@ export default class extends Controller {
 
   disconnect() {
     clearInterval(this.timer)
+  }
+
+  open() {
+    this.overlayTarget.showModal()
+  }
+
+  close() {
+    this.overlayTarget.close()
+  }
+
+  backdropClose(event) {
+    if (event.target === this.overlayTarget) this.close()
   }
 
   next() {
@@ -38,12 +55,10 @@ export default class extends Controller {
 
   show(index) {
     this.index = index
-    this.imageTarget.src = this.urlsValue[index]
-
-    if (this.hasCounterTarget) {
-      this.counterTarget.textContent = `${index + 1} / ${this.urlsValue.length}`
-    }
-
+    this.imageTargets.forEach((image) => (image.src = this.urlsValue[index]))
+    this.counterTargets.forEach((counter) => {
+      counter.textContent = `${index + 1} / ${this.urlsValue.length}`
+    })
     this.preloadNext()
   }
 
