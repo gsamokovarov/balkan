4b5191166b94757a568f4c28e24d10484eb405b3
Genadi Samokovarov <gsamokovarov@gmail.com>
2026-06-23 09:49:54 +0300

    Setup a main color scheme theme-like support
    We have brand colors and style the marketing part of the website to
    them. Every year, I'll change them to keep the website a bit fresh.

--- app/assets/tailwind/tailwind.config.js
+++ app/assets/tailwind/tailwind.config.js
@@ -17,19 +17,6 @@ module.exports = {
       },
       colors: {
         current: "currentColor",
-        brand: {
-          50: "#FDF2F3",
-          100: "#FCE9EB",
-          200: "#F8CED3",
-          300: "#F2ABB2",
-          400: "#EB7F8A",
-          500: "#E03647",
-          600: "#D42133",
-          700: "#B51C2B",
-          800: "#9F1926",
-          900: "#7A131D",
-          950: "#400A0F",
-        },
         x: {
           cyan: "#B1E7F6",
           lime: "#90EE90",
